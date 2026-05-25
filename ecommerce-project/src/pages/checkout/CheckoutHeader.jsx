6d87725047688ec65ede6ce705e3c44fe1e9c3f6
Jasim <jasimpn17@gmail.com>
2026-05-25 08:26:34 +0530

    Link is used instead of <a> attributr for btter reloading. it only added in header componets not in pages

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router'
 import './CheckoutHeader.css'
 export function CheckoutHeader(){
     return(
         <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
-                        <a href="/">
+                        <Link to="/">
                             <img className="logo" src="images/logo.png" />
                             <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </a>
+                        </Link>
                     </div>
 
                     <div className="checkout-header-middle-section">
