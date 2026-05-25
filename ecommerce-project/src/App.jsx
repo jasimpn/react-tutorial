350b0149ab64787a8773910eccbfb52db647f566
Jasim <jasimpn17@gmail.com>
2026-05-25 10:01:08 +0530

    notfoundpage is added for unkown paths

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { HomePage } from './pages/HomePage'
 import {CheckoutPage} from './pages/checkout/CheckoutPage'
 import { OrderPage } from './pages/OrderPage'
 import { TrackingPage } from './pages/TrackingPage'
+import { NotFoundPage } from './pages/NotFoundPage'
 import { Routes , Route} from 'react-router'
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='checkout' element={<CheckoutPage />} />
         <Route path='orders' element={<OrderPage />} />
         <Route path='tracking' element={<TrackingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes> 
     </>
   )
---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+export function NotFoundPage(){
+    return(
+        <>
+            <Header />
+            <div>
+                 <h1>Page not found</h1>s
+            </div>
+           
+        </>
+    )
+}
